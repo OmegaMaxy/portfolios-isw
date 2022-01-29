60b474f3a3af60a607241ccebf88b3407b5b0359
Dazzy <maxys3@hotmail.com>
2022-01-30 00:27:48 +0100

    Update DatabaseSeeder.php

--- portfolios-isw/database/seeders/DatabaseSeeder.php
+++ portfolios-isw/database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,5 +16,9 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\Role::factory(4)->create();
         \App\Models\User::factory(4)->create();
+        foreach (User::all() as $key => $user) {
+            $data = ['user_id' => $user->id, 'page_url' => '/', 'status' => false];
+            \App\Models\Page::create($data);
+        }
     }
 }
