776a56fd3eee52e2f033be17f37cc16c807525e8
Dazzy <maxys3@hotmail.com>
2022-01-30 00:26:42 +0100

    Update overview.blade.php

--- portfolios-isw/resources/views/users/overview.blade.php
+++ portfolios-isw/resources/views/users/overview.blade.php
@@ -1,23 +1,36 @@
-<html>
-    <head></head>
-    <body>
+@extends('layouts.app')
+
+@section('content')
+    <section class="container-fluid mt-3">
         <section>
-            <h1>User Overview!</h1>
-            <p>Description here..</p>
+            <h3>User Overview</h3>
         </section>
         <section>
-            <table>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Username</th>
+                        <th>Name</th>
+                        <th>Role</th>
+                        <th>Page url</th>
+                    </tr>
+                </thead>
                 <tbody>
                     @foreach ($users as $user)
                         <tr>
-                            <td>{{ $user->username }}</td>
-                            <td>{{ $user->fname }}</td>
+                            <td>{{ $user->username }}<a href="{{ $user->linkPath() }}" class="ml-2 badge badge-primary">Details</a></td>
+                            <td>{{ $user->fullname() }}</td>
                             <td>{{ $user->role->name }}</td>
-                            <td>{{ $user->page->url }}</td>
+                            @if ($user->activePage() != null)
+                                <td><a href="{{ $user->activePage()->page_url }}" target="_blank">{{ $user->activePage()->page_url }}</a></td>
+                            @else
+                                <td>User has no active page.</td>
+                            @endif
                         </tr>
                     @endforeach
                 </tbody>
             </table>
+            <a href="{{ url('/users/create') }}" class="btn btn-primary">Add user</a>
         </section>
-    </body>
-</html>
+    </section>
+@endsection
