f77a75d231937ed27ff1b3f973733d792c536d59
dazzy <dazzy.edu@gmail.com>
2022-01-30 03:40:41 +0100

    Routes cleanup

--- portfolios-isw/routes/web.php
+++ portfolios-isw/routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 use \App\Http\Controllers as Controllers;
 
+
+// admin routes
 Route::get('/', [Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/home', [Controllers\HomeController::class, 'index'])->name('home');
 
@@ -12,18 +14,9 @@ Auth::routes();
 Route::resource('users', Controllers\UserController::class)
     ->missing(redirect('users.overview'));
 
-/*
-Route::get('/roles/{roleId}', [App\Http\Controllers\RoleController::class, 'show']);
-Route::get('/roles', [App\Http\Controllers\RoleController::class, 'index'])->name('roles');
-Route::get('/roles/new', [App\Http\Controllers\RoleController::class, 'create']);
-Route::post('/roles', [App\Http\Controllers\RoleController::class, 'store']);
-Route::get('/roles/{roleId}/edit', [App\Http\Controllers\RoleController::class, 'edit']);
-Route::patch('/roles/{roleId}', [App\Http\Controllers\RoleController::class, 'update']);
-Route::delete('/roles/{roleId}', [App\Http\Controllers\RoleController::class, 'destroy']);*/
 
 Route::resource('roles', Controllers\RoleController::class)
     ->missing(redirect('roles.overview'));
-
 
 Route::resource('pages', Controllers\PageController::class)
     //->except('')
