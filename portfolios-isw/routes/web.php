04a9f6a242a646d232963ae84adebd5d021cfca3
dazzy <dazzy.edu@gmail.com>
2022-01-30 04:38:45 +0100

    Updated web.php

--- portfolios-isw/routes/web.php
+++ portfolios-isw/routes/web.php
@@ -6,21 +6,27 @@ use \App\Http\Controllers as Controllers;
 
 
 // admin routes
-Route::get('/', [Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/home', [Controllers\HomeController::class, 'index'])->name('home');
+Route::prefix('admin')->group(
+    function () {
+        Route::get('/', [Controllers\Admin\HomeController::class, 'index'])->name('home');
+        Route::get('/home', [Controllers\Admin\HomeController::class, 'index'])->name('home');
+
+        Route::resource('users', Controllers\Admin\UserController::class)
+            ->missing(redirect('users.overview'));
+
+
+        Route::resource('roles', Controllers\Admin\RoleController::class)
+            ->missing(redirect('roles.overview'));
+
+        Route::resource('pages', Controllers\Admin\PageController::class)
+            //->except('')
+            ->missing(redirect('users.overview'));
+
+        Route::get('/users/{userId}/page/add', [Controllers\Admin\PageController::class, 'create_with_userid']);
+        Route::get('/users/{userId}/page', [Controllers\Admin\PageController::class, 'show']);
+    });
 
 Auth::routes();
+Route::get('/', [Controllers\HomeController::class, 'index'])->name('home');
 
-Route::resource('users', Controllers\UserController::class)
-    ->missing(redirect('users.overview'));
-
-
-Route::resource('roles', Controllers\RoleController::class)
-    ->missing(redirect('roles.overview'));
-
-Route::resource('pages', Controllers\PageController::class)
-    //->except('')
-    ->missing(redirect('users.overview'));
-
-Route::get('/users/{userId}/page/add', [Controllers\PageController::class, 'create_with_userid']);
-Route::get('/users/{userId}/page', [Controllers\PageController::class, 'show']);
+Route::resource('users', Controllers\UserController::class)->only(['index', 'show']);
