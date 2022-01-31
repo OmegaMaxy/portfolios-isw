9afd8a626a02cf5c26ad16d57a59a1c2aa9cb95e
dazzy <dazzy.edu@gmail.com>
2022-01-31 01:34:50 +0100

    Profile Picture is now included in a user tile

--- portfolios-isw/resources/views/components/user-tile.blade.php
+++ portfolios-isw/resources/views/components/user-tile.blade.php
@@ -1,6 +1,11 @@
 <div class="mr-2 mb-2">
     <div class="card" style="width: 18rem;">
-        <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUHNoKb6n8cRAjyputJ9vn4OPdujzLJr52OQ&usqp=CAU" alt="Card image cap">
+        @if (!empty($user->profile_picture))
+        <img class="card-img-top" src="{{ $user->pf() }}" alt="Profile Picture">
+        @else
+            <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUHNoKb6n8cRAjyputJ9vn4OPdujzLJr52OQ&usqp=CAU" alt="Profile Picture">
+        @endif
+
         <div class="card-body">
             <h5 class="card-title">{{ $user->fullname() }}</h5>
             <p class="card-text"><span class="text-secondary"> aka </span>{{ $user->username }} <label class="badge badge-primary">{{ $user->role->name }}</label></p>
