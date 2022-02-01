29698cac4fbeb6995c884f658280b4fd6dcce098
dazzy <dazzy.edu@gmail.com>
2022-02-01 14:35:50 +0100

    Added social handles and icons to portfolio page

--- portfolios-isw/resources/views/users/show.blade.php
+++ portfolios-isw/resources/views/users/show.blade.php
@@ -1,11 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
+    <style>
+        body {
+            background-color: {{ $user->background_color }};
+            color: {{ $user->foreground_color }};
+        }
+        hr {
+            background: {{ $user->foreground_color }};
+        }
+    </style>
     <div class="container">
-        <section class="mt-4 mb-4">
-            <h2>{{ $user->fullname() }} aka {{ $user->username }} | {{ $user->role->name }}</h2>
+        <section class="mt-4 mb-4 d-flex">
+            <div class="mr-5">
+                <img src="{{ $user->pf() }}" class="img-thumbnail rounded" style="height: 150px;" alt="Profile Picture of {{ $user->username }}"/>
+            </div>
+            <div style="align-self: center;">
+                <h2>{{ $user->fullname() }} aka {{ $user->username }}</h2>
+                <p>{{ $user->role->name }}</p>
+            </div>
         </section>
-        <hr>
+        <section>
+            <h3>Social Handles</h3>
+            <div class="btn-group">
+                @foreach (auth()->user()->getHandles() as $handle)
+                    <a href="{{ $handle['url'] }}" class="btn btn-primary mr-3" style="{{ $handle['style'] }}">@svg($handle['icon']) <span style="vertical-align: middle;">{{ $handle['handle'] }}</span></a>
+                @endforeach
+            </div>
+        </section>
+        <hr/>
         <section class="mt-4">
             @parsedown($markdown_portfolio)
         </section>
