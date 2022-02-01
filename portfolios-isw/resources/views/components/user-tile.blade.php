f281838e2f9b7d655cfe1ffbc27eee95ccf2e09f
dazzy <dazzy.edu@gmail.com>
2022-02-01 14:34:26 +0100

    pf() now returns either user's pf or default image defined in User model

--- portfolios-isw/resources/views/components/user-tile.blade.php
+++ portfolios-isw/resources/views/components/user-tile.blade.php
@@ -1,10 +1,6 @@
 <div class="mr-2 mb-2">
     <div class="card" style="width: 18rem;">
-        @if (!empty($user->profile_picture))
         <img class="card-img-top" src="{{ $user->pf() }}" alt="Profile Picture">
-        @else
-            <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUHNoKb6n8cRAjyputJ9vn4OPdujzLJr52OQ&usqp=CAU" alt="Profile Picture">
-        @endif
 
         <div class="card-body">
             <h5 class="card-title">{{ $user->fullname() }}</h5>
